f124bc73c6e28e6f0966f5d1987091b4a9268e1b
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-10-06 18:41:40 +0200

    build

--- Assets/Scripts/ModifyText.cs
+++ Assets/Scripts/ModifyText.cs
@@ -5,6 +5,7 @@ public class ModifyText : MonoBehaviour
 {
 
     public GameObject lowerArmBone;
+    Quaternion Rotation;
     public TMP_Text xRotationText;
     public TMP_Text yRotationText;
     public TMP_Text zRotationText;
@@ -20,6 +21,18 @@ public class ModifyText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetRotations();
+        xRotationText.text = $"X: {Rotation.x}";
+        xRotationText.text = $"Y: {Rotation.y}";
+        xRotationText.text = $"Z: {Rotation.z}";
     }
+
+    public void GetRotations()
+    {
+        lowerArmBone.transform.GetLocalPositionAndRotation(out Vector3 localPosition, out Quaternion localRotation);
+        Rotation.x = localPosition.x;
+        Rotation.y = localPosition.y;
+        Rotation.z = localPosition.z;
+    }
+
 }
