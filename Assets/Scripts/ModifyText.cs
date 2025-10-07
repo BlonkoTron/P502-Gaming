0ea262f04fc8b125ae7fa39344650553e8f183bb
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-10-07 13:39:07 +0200

    hand rotation added

--- Assets/Scripts/ModifyText.cs
+++ Assets/Scripts/ModifyText.cs
@@ -5,34 +5,51 @@ public class ModifyText : MonoBehaviour
 {
 
     public GameObject lowerArmBone;
-    Quaternion Rotation;
+    public GameObject handBone;
+    Quaternion ArmRotation;
+    Quaternion HandRotation;
     public TMP_Text xRotationText;
     public TMP_Text yRotationText;
     public TMP_Text zRotationText;
+    public TMP_Text xHandRotationText;
+    public TMP_Text yHandRotationText;
+    public TMP_Text zHandRotationText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        xRotationText.text = "X: ";
-        yRotationText.text = "Y: ";
-        zRotationText.text = "Z: ";
+        xRotationText.text = "Arm X: ";
+        yRotationText.text = "Arm Y: ";
+        zRotationText.text = "Arm Z: ";
+        xHandRotationText.text = "Hand X: ";
+        yHandRotationText.text = "Hand Y: ";
+        zHandRotationText.text = "Hand Z: ";
     }
 
     // Update is called once per frame
     void Update()
     {
         GetRotations();
-        xRotationText.text = $"X: {Rotation.x}";
-        yRotationText.text = $"Y: {Rotation.y}";
-        zRotationText.text = $"Z: {Rotation.z}";
+        xRotationText.text = $"Arm X: {ArmRotation.x}";
+        yRotationText.text = $"Arm Y: {ArmRotation.y}";
+        zRotationText.text = $"Arm Z: {ArmRotation.z}";
+
+        xHandRotationText.text = $"Hand X: {HandRotation.x}";
+        yHandRotationText.text = $"Hand Y: {HandRotation.y}";
+        zHandRotationText.text = $"Hand Z: {HandRotation.z}";
     }
 
     public void GetRotations()
     {
         lowerArmBone.transform.GetLocalPositionAndRotation(out Vector3 localPosition, out Quaternion localRotation);
-        Rotation.x = localRotation.eulerAngles.x;
-        Rotation.y = localRotation.eulerAngles.y;
-        Rotation.z = localRotation.eulerAngles.z;
+        ArmRotation.x = localRotation.eulerAngles.x;
+        ArmRotation.y = localRotation.eulerAngles.y;
+        ArmRotation.z = localRotation.eulerAngles.z;
+
+        handBone.transform.GetLocalPositionAndRotation(out Vector3 localPositionH, out Quaternion localRotationH);
+        HandRotation.x = localRotationH.eulerAngles.x;
+        HandRotation.y = localRotationH.eulerAngles.y;
+        HandRotation.z = localRotationH.eulerAngles.z;
     }
 
 }
