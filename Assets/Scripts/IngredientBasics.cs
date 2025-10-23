251d27c5fa54018c0d69043da81163b940618c37
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-10-23 14:56:23 +0200

    coding

--- Assets/Scripts/IngredientBasics.cs
+++ Assets/Scripts/IngredientBasics.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using static UnityEngine.ParticleSystem;
+using static UnityEngine.XR.ARSubsystems.XRCpuImage;
 
 public class IngredientBasics : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string ingredientName;
+    public bool isStackable;
+    public GameObject topSnapPoint;
+    public GameObject bottomSnapPoint;
+    private XRBaseInteractable interactable;
+
+    private void Awake()
+    {
+        interactable = GetComponent<XRBaseInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to select event (when picked up)
+        interactable.selectEntered.AddListener(OnSelectEntered);
+        interactable.selectExited.AddListener(OnSelectExited);
+    }
+
+    private void OnDisable()
+    {
+        interactable.selectEntered.RemoveListener(OnSelectEntered);
+        interactable.selectExited.RemoveListener(OnSelectExited);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if(bottomSnapPoint.activeSelf == false)
+        {
+            bottomSnapPoint.SetActive(true);
+        }
+        if(topSnapPoint.activeSelf == false)
+        {
+            topSnapPoint.SetActive(true);
+        }
+    }
+
+    private void OnSelectExited(SelectExitEventArgs args)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
