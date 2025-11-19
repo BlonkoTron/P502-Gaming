3ca81114e62f533667092903a94fd43ce60f2fb6
oscarbagger <oscarbagger@live.dk>
2025-11-19 11:05:13 +0100

    traymanager work

--- Assets/!GAME Project/Scripts/Burger/TrayManager.cs
+++ Assets/!GAME Project/Scripts/Burger/TrayManager.cs
@@ -1,18 +1,54 @@
+using System;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class TrayManager : MonoBehaviour
 {
-    public GameObject platePreview, sodaPreview;
+    public static TrayManager Instance;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameObject plateOnTray,sodaOnTray;
+
+    [SerializeField] private XRSocketInteractor plateSocket,sodaSocket;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    private void Start()
+    {
+        plateSocket.selectEntered.AddListener(SetPlateOnTray);
+        plateSocket.selectExited.AddListener(RemovePlateOnTray);
+        sodaSocket.selectEntered.AddListener(SetSodaOnTray);
+        sodaSocket.selectExited.AddListener(RemoveSodaOnTray);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RemoveSodaOnTray(SelectExitEventArgs arg0)
     {
-        
+        sodaOnTray = null;
+    }
+
+    private void SetSodaOnTray(SelectEnterEventArgs arg0)
+    {
+        sodaOnTray = arg0.interactableObject.transform.gameObject;
+        Debug.Log(sodaOnTray);
+    }
+
+    private void RemovePlateOnTray(SelectExitEventArgs arg0)
+    {
+        plateOnTray = null;
+    }
+
+    private void SetPlateOnTray(SelectEnterEventArgs arg0)
+    {
+        plateOnTray = arg0.interactableObject.transform.gameObject;
+        Debug.Log(plateOnTray);
     }
 }
