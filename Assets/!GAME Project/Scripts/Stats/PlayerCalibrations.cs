3f087f828ac5aba6414e169484bdfb2a9ecc2474
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-11-19 13:51:25 +0100

    Bent stuff

--- Assets/!GAME Project/Scripts/Stats/PlayerCalibrations.cs
+++ Assets/!GAME Project/Scripts/Stats/PlayerCalibrations.cs
@@ -4,14 +4,6 @@ public class PlayerCalibrations : MonoBehaviour
 {
     [SerializeField] private PlayerSetUp playerSetUp;
 
-    private void Start()
-    {
-        
-    }
-
-    private void CalculateThresholds()
-    {
-
-    }
+   
 
 }
