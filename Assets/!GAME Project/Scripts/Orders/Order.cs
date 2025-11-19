0f7e3bd8078a93769fcbef5479c93c621867bf0f
oscarbagger <oscarbagger@live.dk>
2025-11-19 12:45:32 +0100

    changed drink types

--- Assets/!GAME Project/Scripts/Orders/Order.cs
+++ Assets/!GAME Project/Scripts/Orders/Order.cs
@@ -20,8 +20,8 @@ public class Order
     }
     public enum Drink {
         none,
-        red,
-        pink
+        Moon_juice,
+        Nebula_blast
     }
 
     public List<BurgerIngredient> Burger;
@@ -30,6 +30,7 @@ public class Order
 
     public bool CheckBurgerMatch(List<BurgerIngredient> otherBurger)
     {
+        if (otherBurger==null) { return false; }
         var count = 0;
         foreach (var item in otherBurger)
         {
