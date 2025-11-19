2df58d85f847491cd38654ab796b1e494d84f87e
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-11-19 15:01:49 +0100

    rotation start

--- Assets/!GAME Project/Scripts/Stats/TrackRotations.cs
+++ Assets/!GAME Project/Scripts/Stats/TrackRotations.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class TrackRotations : MonoBehaviour
 {
-    
+    [SerializeField] private PlayerSetUp playerSetUp;
+    [SerializeField] private PlayerStats playerStats;
+
+
 }
