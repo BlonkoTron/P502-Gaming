78709f95195fb1a3a3c41ed5365f712d2dcc3345
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-11-20 15:02:35 +0100

    dropdown setup

--- Assets/!GAME Project/Scripts/Stats/PlayerCalibrations.cs
+++ Assets/!GAME Project/Scripts/Stats/PlayerCalibrations.cs
@@ -1,9 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCalibrations : MonoBehaviour
 {
     [SerializeField] private PlayerSetUp playerSetUp;
 
-   
+    [SerializeField] private Dropdown bentInDrop;
+    [SerializeField] private Dropdown bentOutDrop;
+    [SerializeField] private Dropdown rotationUpDrop;
+    [SerializeField] private Dropdown rotationDownDrop;
+
+
+    public void Start()
+    {
+        Debug.Log(bentInDrop.value);
+    }
+
+
+
+
+    public void SetPlayerSetUp()
+    {
+
+    }
+
+
+    public void UpdateBentROMIn()
+    {
+
+       
+         switch (bentInDrop.value)
+         {
+             case 0:
+                    playerSetUp.bentROMIn = 20f;
+                    break;
+             case 1:
+                    playerSetUp.bentROMIn = 25f;
+                    break;
+             case 2:
+                    playerSetUp.bentROMIn = 35f;
+                    break;
+             case 3:
+                    playerSetUp.bentROMIn = 45f;
+                    break;
+        }
+    }
 
 }
