1329f176b3f785f73608d11b12be6584e31341ee
oscarbagger <oscarbagger@live.dk>
2025-11-21 10:31:33 +0100

    order counter

--- Assets/!GAME Project/Scripts/Orders/OrderFeedbackUI.cs
+++ Assets/!GAME Project/Scripts/Orders/OrderFeedbackUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class OrderFeedbackUI : MonoBehaviour
 {
@@ -9,17 +10,18 @@ public class OrderFeedbackUI : MonoBehaviour
     [SerializeField] private Image OrderFullfillImage;
     [SerializeField] private GameObject OrderFullFilledPanel;
     [SerializeField] private Sprite orderCorrectSprite, orderWrongSprite;
+    [SerializeField] private TMP_Text orderCountText;
     private float panelAnimationTime = 4;
     void Start()
     {
         orderController = OrderController.Instance;
         orderController.OnOrderFullfilled.AddListener(OrderController_OnOrderFullfilled);
         OrderFullFilledPanel.SetActive(false);
+        UpdateOrderCountText();
     }
     private void OnDestroy()
     {
         orderController.OnOrderFullfilled.RemoveListener(OrderController_OnOrderFullfilled);
-
     }
     private void OrderController_OnOrderFullfilled(bool correctOrder)
     {
@@ -31,11 +33,19 @@ public class OrderFeedbackUI : MonoBehaviour
         {
             OrderFullfillImage.sprite = orderWrongSprite;
         }
+        UpdateOrderCountText();
     }
     private IEnumerator OrderFullfillpanelAnimate()
     {
         OrderFullFilledPanel.SetActive(true);
         yield return new WaitForSeconds(panelAnimationTime);
         OrderFullFilledPanel.SetActive(false);
+    }
+    private void UpdateOrderCountText()
+    {
+        if (orderCountText != null)
+        {
+            orderCountText.text = orderController.correctOrdersServed.ToString();
+        }
     }
 }
