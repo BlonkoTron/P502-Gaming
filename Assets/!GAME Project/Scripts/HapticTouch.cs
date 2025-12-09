97922de6d56ecf3468c3e06d78220c9e688c249e
VHSTapeDVD <axschoenning@gmail.com>
2025-12-09 14:03:03 +0100

    hm

---
+++ Assets/!GAME Project/Scripts/HapticTouch.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class HapticTouch : MonoBehaviour
+{
+    [Header("Haptic Settings")]
+    [SerializeField] private float hapticIntensity = 0.5f;
+    [SerializeField] private float hapticDuration = 0.2f;
+
+    private XRGrabInteractable grabInteractable;
+
+    void Start()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrab);
+            Debug.Log("HapticTouch: Listener added to " + gameObject.name);
+        }
+        else
+        {
+            Debug.LogError("HapticTouch: No XRGrabInteractable found on " + gameObject.name);
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log("HapticTouch: Object grabbed!");
+        
+        // Get the interactor (controller) that grabbed this object
+        if (args.interactorObject is XRBaseInputInteractor inputInteractor)
+        {
+            Debug.Log("HapticTouch: Sending haptic impulse - Intensity: " + hapticIntensity + ", Duration: " + hapticDuration);
+            // Send haptic impulse to the controller
+            inputInteractor.SendHapticImpulse(hapticIntensity, hapticDuration);
+        }
+        else
+        {
+            Debug.LogWarning("HapticTouch: Interactor is not XRBaseInputInteractor, it is: " + args.interactorObject.GetType());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.RemoveListener(OnGrab);
+        }
+    }
+}
